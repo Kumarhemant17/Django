668c4b35a6612fc4f58380f6c9970eda5f8fd1d7
Hemant Kumar <kumar.hemant1797@gmail.com>
2026-03-25 16:41:08 +0530

    Initial Django Project Setup

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Student
+
+# Create your views here.
+# Views are functions or classes that handle requests and return responses
+def home(request):
+    students=Student.objects.filter(age__gt=22).order_by('name')
+    return render(request,'home/index.html', {'students':students})
