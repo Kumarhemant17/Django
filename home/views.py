3e042a3e1f5c16f38caf46934e4122e4fe894f07
Hemant Kumar <kumar.hemant1797@gmail.com>
2026-03-26 23:22:49 +0530

    Improved UI and optimize student query

--- home/views.py
+++ home/views.py
@@ -3,11 +3,11 @@ from .models import Student
 
 def home(request):
     students = Student.objects.filter(age__gt=22).order_by('name')
-    students_count = Student.objects.count()
+    students_count = students.count()   # improved
     student_name = Student.objects.order_by('name')
 
     return render(request, 'home/index.html', {
         'students': students,
-        'students_count': students_count,
-        'student_name': student_name
+        'students_count': students.count(),
+        'student_name': all_students
     })
