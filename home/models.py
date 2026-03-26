cb680f4b439705e9859a990552279c8500aaab4d
Hemant Kumar <kumar.hemant1797@gmail.com>
2026-03-26 23:07:43 +0530

    Fix bug in PR

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ class Student(models.Model):
     name=models.CharField(max_length=100)
     age=models.IntegerField()
     email=models.EmailField()
+    
     created_at=models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
